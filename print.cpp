3b481df35c5dc4aa2f38bdaeae3b6b94cc03b88d
Pranshu321 <pranshujain0111@gmail.com>
2021-10-07 16:58:52 +0530

    modified file

--- print.cpp
+++ print.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace std;
+int addnum(int n){
+   if(n==0)
+   return 0;
+   return n+addnum(n-1);
+}
 int main(){
-    printf("Hi pranshu go on yes now rith c");
+    int n; cin>>n;
+      cout<<addnum(n);
     return 0;
 }
